7e392887ceaee5ddabb1d9fca984be577e9775b0
Matt Gagnon <mattjgagnon@gmail.com>
2020-08-02 15:52:37 -0700

    Implement tdd for area shapes

--- perimeter_tdd/perimeter_test.go
+++ perimeter_tdd/perimeter_test.go
@@ -4,20 +4,24 @@ import (
 	"testing"
 )
 
-func TestPerimeter(t *testing.T)  {
-	got  := Perimeter(10.0, 10.0)
-	want := 40.0
+func TestArea(t *testing.T) {
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := rectangle.Area()
+		want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}	
-}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 
-func TestArea(t *testing.T)  {
-	got  := Area(12.0, 6.0)
-	want := 72.0
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}	
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
