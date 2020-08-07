e7210214be09c2d2d9f3c7343a113fdc74cdcf23
Matt Gagnon <mattjgagnon@gmail.com>
2020-08-06 20:07:25 -0700

    Refactor test into run suite

--- pointers_tdd/pointers_test.go
+++ pointers_tdd/pointers_test.go
@@ -5,12 +5,14 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
-	got := wallet.Balance()
-	want := Bitcoin(10)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
